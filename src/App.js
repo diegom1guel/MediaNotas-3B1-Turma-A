e5ca149ad1df30503f3546aa45c8de2be28de855
Aluno <aluno@etec.sp.gov.br>
2023-03-06 08:53:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/MediaNotas.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MediaNotas nota1="5" nota2="7" nota3="9"></MediaNotas>
     </div>
   );
+  
+
+  
 }
 
+
+
 export default App;
+
